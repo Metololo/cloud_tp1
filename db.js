7d3b65f1696af6fb17dc9573964b5f1ebb9cf23e
menfou <samy@SHEEESH>
2024-10-30 15:04:49 +0100

    add db

--- db.js
+++ db.js
@@ -2,15 +2,21 @@ const { Sequelize } = require('sequelize')
 
 // Database
 const sequelize = new Sequelize(
-  '', // TODO: database connection string
   {
     dialect: 'postgres',
+    database:'',
+
     dialectOptions: {
       ssl: {
         require: true,
         rejectUnauthorized: false,
       },
     },
+    database: process.env.DB_NAME,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     define: {
       createdAt: 'added',
       updatedAt: 'updated',
